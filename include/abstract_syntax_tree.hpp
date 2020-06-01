6ae9ea0c7ec5110474fea9c304898f908d00de6f
Vincent Reverdy <vince.rev@gmail.com>
2020-06-01 19:10:13 +0200

    Added first files.

---
+++ include/abstract_syntax_tree.hpp
@@ -0,0 +1,28 @@
+// ========================== ABSTRACT SYNTAX TREE ========================== //
+// Project:         SymboliC++
+// Name:            abstract_syntax_tree.hpp
+// Description:     An abstract syntax tree to manipulate symbolic formulas
+// Creator:         Vincent Reverdy
+// Contributor(s):  Vincent Reverdy [2020-]
+// License:         BSD 3-Clause License
+// ========================================================================== //
+#ifndef _ABSTRACT_SYNTAX_TREE_HPP_INCLUDED
+#define _ABSTRACT_SYNTAX_TREE_HPP_INCLUDED
+// ========================================================================== //
+
+
+
+// ============================== PREAMBLE ================================== //
+// C++ standard library
+// Project sources
+// Third-party libraries
+// Miscellaneous
+namespace symbolic {
+// ========================================================================== //
+
+
+
+// ========================================================================== //
+} // namespace symbolic
+#endif // _ABSTRACT_SYNTAX_TREE_HPP_INCLUDED
+// ========================================================================== //
---
+++ include/symbolic.hpp
@@ -0,0 +1,28 @@
+// ================================ SYMBOLIC ================================ //
+// Project:         SymboliC++
+// Name:            symbolic.hpp
+// Description:     Main header to include the entire symbolic library
+// Creator:         Vincent Reverdy
+// Contributor(s):  Vincent Reverdy [2020-]
+// License:         BSD 3-Clause License
+// ========================================================================== //
+#ifndef _SYMBOLIC_HPP_INCLUDED
+#define _SYMBOLIC_HPP_INCLUDED
+// ========================================================================== //
+
+
+
+// ============================== PREAMBLE ================================== //
+// C++ standard library
+// Project sources
+// Third-party libraries
+// Miscellaneous
+namespace symbolic {
+// ========================================================================== //
+
+
+
+// ========================================================================== //
+} // namespace symbolic
+#endif // _SYMBOLIC_HPP_INCLUDED
+// ========================================================================== //
